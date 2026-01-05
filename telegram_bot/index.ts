27101927c9c923d212127279d8398e768912b591
MrBlackGhostt <hemantkumar4213@icloud.com>
2026-01-05 19:34:12 +0530

    got the balance for the wallet test on the devnet

--- telegram_bot/index.ts
+++ telegram_bot/index.ts
@@ -1,7 +1,7 @@
 import TelegramBot from "node-telegram-bot-api";
-import { Connection, PublicKey } from "@solana/web3.js";
+import { Connection, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
 
-const connection = new Connection("https://api.mainnet-beta.solana.com");
+const connection = new Connection("https://api.devnet.solana.com");
 
 const token = process.env.BOT_TOKEN;
 
@@ -27,22 +27,32 @@ if (!token) {
       },
     });
   });
-  bot.onText(/\/balance/, (msg,match) => {
-  if(match == null) return bot.sendMessage(msg.chat.id, "Provide the Key")
-    bot.sendMessage(msg.chat.id, "Your Balance is 1 Billion Euro");
-const walletAddress = match[1]
-    try {
-     const pubKey = new PublicKey(walletAddress!) 
-     const balance = 
-    } catch (error) {
-      
+  bot.onText(/\/balance (.+)/, async (msg, match) => {
+    if (match == null || !match[1]) {
+      return bot.sendMessage(msg.chat.id, "Give the key");
     }
-  bot.onText(/\/sendpic/, (msg) => {
-    bot.sendPhoto(msg.chat.id, "https://www.somesite.com/image.jpg", {
-      caption: "Here we go ! \nThis is just a caption ",
+    const walletAddress = match[1];
+    try {
+      const pubKey = new PublicKey(walletAddress);
+      const balance = await connection.getBalance(pubKey);
+      const solBalance = balance / LAMPORTS_PER_SOL;
+
+      bot.sendMessage(
+        msg.chat.id,
+        `Wallet ${walletAddress}\Balance: ${solBalance}`,
+      );
+    } catch (error) {
+      bot.sendMessage(
+        msg.chat.id,
+        `Invalid Wallet Address or error fetching balance ${walletAddress}`,
+      );
+    }
+    bot.onText(/\/sendpic/, (msg) => {
+      bot.sendPhoto(msg.chat.id, "https://www.somesite.com/image.jpg", {
+        caption: "Here we go ! \nThis is just a caption ",
+      });
     });
   });
-
   // Listen for any kind of message. There are different kinds of
   // messages.
   bot.on("message", (msg) => {
